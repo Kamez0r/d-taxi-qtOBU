d14751e58036d67a3a90a9023240cbf08fddc2c1
Alexandru-Ștefan PĂNESCU <alexandru.panescu@stud.trans.upb.ro>
2025-06-12 13:50:46 +0300

    boilerplate

---
+++ src/gui/MainWindow.py
@@ -0,0 +1,12 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QMainWindow, QLabel
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Hello, PyQt")
+        self.setMinimumSize(400, 300)
+
+        label = QLabel("Hello, World!", self)
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setCentralWidget(label)
